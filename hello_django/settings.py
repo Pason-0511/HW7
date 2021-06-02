7b32e1d2e2776cf4943ec01f8022c1f695e50745
Pason-0511 <Pason0511@gmail.com>
2021-06-02 18:26:04 +0800

    first commit

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4$w(w$9m=jv%75gp52zt!2s0_m_)c09^-6g9bxx%t^p)wpr7x#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','hw77777.herokuapp.com']
 
 
 # Application definition
