f91d7ecbf98b51df8d5eed6df2a83cf8da63232f
Pason-0511 <78478212+Pason-0511@users.noreply.github.com>
2021-06-02 20:54:38 +0800

    Add files via upload

--- hello_django/urls.py
+++ hello_django/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import re_path
+from django.urls import path
 from django.conf.urls import include
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
 
-urlpatterns += [
-    re_path('myhello/', include('myhello.urls')),
+urlpatterns = [
+    path(r'', include('myhello.urls')),
+    path(r'', admin.site.urls),
 ]
